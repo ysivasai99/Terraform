97db6479b29192d9d471edee04ef7609baf782e2
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2023-12-18 13:32:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,33 @@
 provider "aws" {
         region = "us-east-2"
 }
-    resource "aws_instance" "newterraform" {
-        ami = "ami-09f85f3aaae282910"
-        instance_type = "t2.micro"
+#vpc
+resource "aws_vpc" "Projectvpc" {
+        cidr_block = "10.0.0.0/16"
         tags = {
-                Name = "newterraform"
+                Name = "Projectvpc"
 }
 }
+#public subnets
+resource "aws_subnet" "public-subnet-1" {
+        vpc_id = aws_vpc.Projectvpc.id
+        cidr_block = "10.0.1.0/24"
+        availability_zone = : "us-east-2a"
+        map_public_ip_on_launch = "true"
+        tags = {
+                Name = "public-subnet-1"
+}
+}
+resource "aws_subnet" "public-subnet-2" {
+        vpc_id = aws_vpc.Projectvpc.id
+        cidr_block = "10.0.1.0/24"
+        availability_zone = "us-east-2b"
+        map_public_ip_on_launch = "true"
+        tags = {
+                Name = "public-subnet-2"
+}
+}
+# route tables
+resource "aws_route
+        
+
