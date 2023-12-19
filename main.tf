94ac9d4801c8f9ab7dc772cdb5fd737485e7f00e
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2023-12-19 11:00:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,25 +9,62 @@ resource "aws_vpc" "Projectvpc" {
 }
 }
 #public subnets
-resource "aws_subnet" "public-subnet-1" {
+resource "aws_subnet" "public-subnet-2a" {
         vpc_id = aws_vpc.Projectvpc.id
         cidr_block = "10.0.1.0/24"
         availability_zone = : "us-east-2a"
         map_public_ip_on_launch = "true"
         tags = {
-                Name = "public-subnet-1"
+                Name = "public-subnet-2a"
 }
 }
-resource "aws_subnet" "public-subnet-2" {
+resource "aws_subnet" "public-subnet-2b" {
         vpc_id = aws_vpc.Projectvpc.id
-        cidr_block = "10.0.1.0/24"
+        cidr_block = "10.0.3.0/24"
         availability_zone = "us-east-2b"
         map_public_ip_on_launch = "true"
         tags = {
-                Name = "public-subnet-2"
+                Name = "public-subnet-2b"
 }
 }
-# route tables
-resource "aws_route
+# Private Subnets
+resource "aws_subnet" "private-subnet-2a" {
+        vpc_id = aws_vpc.Projectvpc.id
+        cidr_block = "10.0.2.0/24"
+        availability_zone = "us-east-2a"
+        map_public_ip_on_launch = "false"
+        tags = {
+                Name = "private-subnet-2a"
+}
+}
+resource " aws_subnet" "private-subnet-2b" {
+        vpc_id = aws_vpc.project.id
+        cidr_block = "10.0.4.0/24"
+        availability_zone = "us-east-2b"
+        map_public_ip_on_launch = "false"
+        tags = {
+                Name = "private-subnet-2b"
+}
+}
+# internet gateway
+resource "aws_internet_gateway" "myig" {
+        vpc_id = aws_vpc.Projectvpc.id
+        tags = {
+                Name = "myig"
+}
+}
+# Route Table
+resource "aws_route_table" "web_rt" {
+        vpc_id = aws_vpc.Projectvpc.id
+        route {
+                cidr_block = "0.0.0.0/0"
+                gateway_id = aws_internet_gateway.myig.id
+}
+        tags = {
+                Name = "web_rt"
+}
+}
+resource "aws_route_table" "app-rt" {
+        vpc_id = aws_vpc_
         
 
